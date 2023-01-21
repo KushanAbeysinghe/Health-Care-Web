8398983c6468a8020cbda95b78210b4a4307e9dc
KushanAbeysinghe <kushjayamal@gmail.com>
2023-01-21 19:34:32 +0530

    Error correct with Interface

---
+++ AngularApp/src/app/interfaces/order.ts
@@ -0,0 +1,10 @@
+export interface Order {
+    // _id: string,
+    name: string 
+    surname:String,
+    address:String,
+    email:String,
+    phone:String,
+    age:String,
+    image:String
+}
